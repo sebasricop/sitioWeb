8f791d4c77a53acadb9c2a17082deb87be440137
sebasricop <sebastianrp21@hotmail.com>
2024-05-13 18:24:32 -0500

    first commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+
+echo "Hola GUADALUPE";
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sitio Web</title>
+</head>
+
+<body>
+
+    lAS INBAÑABLES
+
+</body>
+
+</html>
