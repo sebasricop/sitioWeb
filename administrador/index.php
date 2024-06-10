b766be511318158fea3ee3b27701e5902acc6907
sebasricop <sebastianrp21@hotmail.com>
2024-06-09 20:44:42 -0500

    Advance inicio.php

--- administrador/index.php
+++ administrador/index.php
@@ -1,3 +1,11 @@
+<?php 
+
+if($_POST){
+    header('Location:inicio.php');
+}
+
+?>
+
 <!doctype html>
 <html lang="en">
 
@@ -12,7 +20,11 @@
 <body>
     <div class="container">
         <div class="row">
-            <div class="col-md-12">
+            <div class="col-md-4">
+                
+            </div>
+            <div class="col-md-4">
+                <br><br><br>
 
                 <div class="card">
                     <div class="card-header">
@@ -20,21 +32,19 @@
                     </div>
                     <div class="card-body">
 
-                    <form>
+                    <form method="POST">
+
                     <div class = "form-group">
-                    <label for="exampleInputEmail1">Email address</label>
-                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-                    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+                    <label>Usuario</label>
+                    <input type="text" class="form-control" name="usuario" placeholder="Escribe tu usuario">
                     </div>
+
                     <div class="form-group">
-                    <label for="exampleInputPassword1">Password</label>
-                    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-                    </div>
-                    <div class="form-check">
-                    <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                    <label class="form-check-label" for="exampleCheck1">Check me out</label>
-                    </div>
-                    <button type="submit" class="btn btn-primary">Sign In</button>
+                    <label>Contraseña:</label>
+                    <input type="password" class="form-control" name="contrasenia" placeholder="Escribe tu contraseña">
+                    </div>                   
+
+                    <button type="submit" class="btn btn-primary">Entrar al administrador</button>
                     </form>
                     
                     
